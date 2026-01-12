97dde68f3e1def89c5753f512f6f6197a33fd03d
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2026-01-12 11:26:20 +0300

    Update main urls

---
+++ pesapal/pesapal_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('users/', views.users_view, name='users'),
+    path('users/add/', views.add_user, name='add_user'),
+    path('users/edit/<int:user_id>/', views.edit_user, name='edit_user'),
+    path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),
+    path('products/', views.products_view, name='products'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('api/query/', views.api_query, name='api_query'),
+    path('api/schema/', views.api_schema, name='api_schema'),
+    path('join/', views.run_join, name='join_demo'),
+]
